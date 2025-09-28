41f1e06d0b3705037d6359371c8cec6e235b8e1b
Eren Albayrak <149527247+Earles0@users.noreply.github.com>
2025-09-29 01:48:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ class NuzlockeTracker {
         
         // MANUAL EDITING: Add hero names here to mark them as dead
         // Example: this.deadHeroes = ['Abrams', 'Bebop', 'Billy'];
-        this.deadHeroes = ['Abrams'];
+        this.deadHeroes = [];
         
         this.heroStates = this.loadHeroStates();
         
@@ -231,6 +231,7 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('- nuzlockeTracker.resetAllHeroes() - Reset all heroes to alive');
     console.log('- nuzlockeTracker.toggleAdminMode() - Toggle admin mode');
 });
+
 
 
 
